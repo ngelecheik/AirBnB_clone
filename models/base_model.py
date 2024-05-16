9b6fc2ce9d40ce830ffe729d778229c9db4e1b21
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-16 15:28:48 +0300

    updated to_dict to show the dates in iso_format

--- models/base_model.py
+++ models/base_model.py
@@ -21,6 +21,8 @@ class BaseModel:
         """returns a dictionary containing
         all keys/values of __dict__ of the instance"""
         my_dict = self.__dict__
+        my_dict['created_at'] = my_dict['created_at'].isoformat()
+        my_dict['updated_at'] = my_dict['updated_at'].isoformat()
         my_dict['__class__'] = self.__class__.__name__
         return my_dict
 
