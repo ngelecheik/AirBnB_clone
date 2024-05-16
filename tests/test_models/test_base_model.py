e80582ed34e634b517293decb63550a78b52bd19
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-16 20:25:22 +0300

    added testing the save

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,7 +9,10 @@ class TestBaseModel(unittest.TestCase):
     """Tests all classes and functionality"""
     def test_save(self):
         'test the save function'
-        pass
+        basemodel = BaseModel()
+        intial_time = basemodel.updated_at
+        updated_time = basemodel.save()
+        self.assertNotEqual(intial_time, updated_time)
 
     def test_todict(self):
         'test the todict() function'
