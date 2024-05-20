c8751206967b457308faf7be3a7c90d845a1ecf3
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-20 13:46:41 +0300

    perfected the update function

--- console.py
+++ console.py
@@ -40,7 +40,8 @@ class HBNBCommand(cmd.Cmd):
                 print("** instance id missing ** ")
                 return
             if key in all_objs:
-                print(str(all_objs[key]))
+                my_object_s = BaseModel(**all_objs[key])
+                print(str(my_object_s))
             else:
                 print("** no instance found **")
                 return
@@ -79,16 +80,67 @@ class HBNBCommand(cmd.Cmd):
         """Prints all string representation of
          all instances based or not on
           the class name. Ex: $ all BaseModel or $ all"""
-        all_list = []
+
         if not line:
             print("** class name missing **")
             return
         all_objs = storage.all()
         if line == "BaseModel":
+            all_list = []
             for value in all_objs.values():
                 new = BaseModel(**value)
                 all_list.append(str(new))
-        print(all_list)
+            print(all_list)
+        else:
+            print("** class doesn't exist **")
+
+    def do_update(self, line):
+        """Updates an instance based on the
+         class name and id by adding or updating
+          attribute (save the change into the JSON file)"""
+        if not line:
+            print("** class name missing **")
+            return
+        args = line.split()
+        all_objs = storage.all()
+        if args[0] == "BaseModel":
+            if len(args) >= 2:
+                key = f"BaseModel.{args[1]}"
+            else:
+                print("** instance id missing ** ")
+                return
+            if len(args) < 3:
+                print("** attribute name missing **")
+                return
+
+            if len(args) < 4:
+                print("** value missing **")
+                return
+            if key in all_objs:
+                # get the object that I will change
+                changing_object = all_objs[key]
+                # reomve the present one
+                del all_objs[key]
+                # bring life to the object from dict
+                my_object = BaseModel(**changing_object)
+                # set the third attribute with 4 value
+                value = args[3]
+                value = value.strip('"')
+                setattr(my_object, args[2], value)
+                # return the object to dictionary
+                my_object_dict = my_object.to_dict()
+                # add it to the retrived dictionary
+                all_objs[key] = my_object_dict
+                # update the json
+                objects_json = json.dumps(all_objs)
+                with open('file.json', 'w', encoding='utf-8') as f:
+                    f.write(objects_json)
+            else:
+                print("** no instance found **")
+                return
+        else:
+            print("** class doesn't exist **")
+            return
 
     def do_quit(self, line):
         "Quit command to exit the program"
