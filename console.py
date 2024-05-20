fefd4aca04e9b650f1704a5dff302ed25217dd86
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-20 09:26:42 +0300

    can create, can quit this are the commands that are working

--- console.py
+++ console.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
 """This module is the entry point of the program"""
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
     """the entry point of the program"""
     prompt = '(hbnb)'
+
+    def do_create(self, line):
+        """reates a new instance of BaseModel,
+         saves it (to the JSON file) and prints the id.
+         Ex: $ create BaseModel"""
+        if not line:
+            print("** class name missing **")
+            return
+        if line == "BaseModel":
+            my_model = BaseModel()
+            my_model.save()
+            print(my_model.id)
+        else:
+            print("** class doesn't exist **")
 
     def do_quit(self, line):
         "Quit command to exit the program"
