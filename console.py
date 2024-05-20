7bcda020e7cd3464dcf7e17b23a4cbeaeb3508ef
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-20 10:04:15 +0300

    aded the show command and tested

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """This module is the entry point of the program"""
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -21,6 +22,30 @@ class HBNBCommand(cmd.Cmd):
             print(my_model.id)
         else:
             print("** class doesn't exist **")
+
+    def do_show(self, line):
+        """Prints the string representation of an
+         instance basedon the class name and id.
+          Ex: $ show BaseModel 1234-1234-1234"""
+        if not line:
+            print("** class name missing **")
+            return
+        args = line.split()
+        all_objs = storage.all()
+        if args[0] == "BaseModel":
+            if len(args) == 2:
+                key = f"BaseModel.{args[1]}"
+            else:
+                print("** instance id missing ** ")
+                return
+            if key in all_objs:
+                print(str(all_objs[key]))
+            else:
+                print("** no instance found **")
+                return
+        else:
+            print("** class doesn't exist **")
+            return
 
     def do_quit(self, line):
         "Quit command to exit the program"
