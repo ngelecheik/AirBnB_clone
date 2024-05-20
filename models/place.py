356ec2e37769b2e5815a4be29bba7c6f642c3950
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-20 16:05:28 +0300

    corrected the data types of attributes

--- models/place.py
+++ models/place.py
@@ -8,10 +8,10 @@ class Place(BaseModel):
     user_id = ""
     name = ""
     description = ""
-    number_rooms = ""
-    number_bathrooms = ""
-    max_guest = ""
-    price_by_night = ""
-    latitude = ""
-    longitude = ""
-    amenities = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
