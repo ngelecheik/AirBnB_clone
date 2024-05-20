4d4c274939f7d786f918a65bdbce2c96fac7c09a
ngelecheikiprop <ngelecheikiprop@gmail.com>
2024-05-20 09:09:47 +0300

    the begining of my own console;

--- console.py
+++ console.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """This module is the entry point of the program"""
+import cmd
 
 
-def main():
-    """the entruy point of the program"""
-    pass
+class HBNBCommand(cmd.Cmd):
+    """the entry point of the program"""
+    prompt = '(hbnb)'
+
+    def do_quit(self, line):
+        "Quit command to exit the program"
+        return True
+
+    def do_EOF(self, line):
+        "Exit program"
+        return True
+
+    def emptyline(self):
+        pass
 
 
-main()
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
